ef379e3a089a8884cf397d9ae4c7885686407b48
alexandre domingos <alexandre@webmail.com.br>
2025-04-13 10:25:36 -0300

    Escolhendo a opção CNPJ e inserindo os dados CNPJ, login, senha

---
+++ cypress/e2e/smartlogweb.cy.js
@@ -0,0 +1,28 @@
+describe('Inobag|BackOffice', () => {
+  beforeEach(() => {
+    cy.visit('https://portal.smartlogweb.com')
+  })
+
+  it('Preenchimento de login com CNPJ', () => {
+    // Habilita o campo de CNPJ
+    cy.get('#ext-element-37').click()
+
+    // Força preenchimento do CNPJ mesmo com máscara
+    cy.get('#ext-element-55').then($input => {
+      const el = $input[0];
+      el.value = '07526557000100';
+      el.dispatchEvent(new Event('input', { bubbles: true }));
+      el.dispatchEvent(new Event('change', { bubbles: true }));
+      el.dispatchEvent(new Event('blur', { bubbles: true }));
+    })
+
+    // Preenche usuário e senha
+    cy.get('#ext-element-68').type('monica.anderson', { delay: 100 }).blur()
+    cy.get('#ext-element-80').type('LD%v+@zp2ME7DH*b', { delay: 100 }).blur()
+
+    // Aguarda o botão estar habilitado e clica nele
+    cy.get('button[data-componentid="ext-button-1"]')
+      .should('not.be.disabled')
+      .click({ force: true })
+  })
+})
