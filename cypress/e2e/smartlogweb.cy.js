8808c90352e43ae476ac82f22ab5cc09334d2923
alexandre domingos <alexandre@webmail.com.br>
2025-04-13 21:43:49 -0300

    Inserindo código para solucionar o problema de não carregamentos dos produtos a separar

--- cypress/e2e/smartlogweb.cy.js
+++ cypress/e2e/smartlogweb.cy.js
@@ -31,7 +31,7 @@
     cy.get('#ext-element-226').click({ force: true });
 
     // Adicionar uma separação
-    cy.get('#ext-element-357').click()
+    cy.get('#ext-element-357', { timeout: 10000 }).click();
 
  // Aguarda o botão do combo de separação aparecer e clica
 cy.get('#ext-element-473', { timeout: 10000 }).should('be.visible').click({ force: true });
@@ -43,16 +43,33 @@ cy.get('#ext-element-641', {timeout: 15000 }).should('be.visible');
 cy.get('[id^="ext-simplelistitem"]').contains('Manual').click({ force: true });
 
 // Insere o local da baixa
-cy.get('#ext-expandtrigger-3 > .x-icon-el', {timeout: 10000 }).type('Moon').click();
+cy.get('#ext-expandtrigger-3 > .x-icon-el', {timeout: 10000 }).type('Expo II').click();
 
 // Insere o colobarador 
 //cy.get('#ext-expandtrigger-4 > .x-icon-el').type('monica.anderson', { delay: 200 }).blur();
-cy.get('#ext-element-504',  {timeout: 15000 }).type('Monica', { delay: 200 }).blur();
+cy.get('#ext-element-504',  {timeout: 15000 }).type('Monica', { delay: 300 }).blur();
 cy.get('[id^="ext-simplelistitem"]', { timeout: 10000 })
   .should('contain.text', 'Monica');
 
 // Clica no botão +Adiconar Itens quando o mesmo for habilitado
-cy.get('#ext-element-600').click();
+cy.get('#ext-element-600').click({ force: true });
+
+
+cy.get('body').then(($body) => {
+  if ($body.find('h2:contains("Os itens ainda não podem ser separados")').length > 0) {
+    cy.log('Mensagem de bloqueio exibida');
+  } else {
+    cy.get('#ext-container-13').should('not.have.class', 'x-hidden-display');
+  }
+});
+
+// cy.intercept('GET', '**/api/separations/products**').as('carregarProdutos');
+
+// cy.get('#ext-element-600').click({ force: true }); // botão de abrir a view
+// cy.wait('@carregarProdutos');
+
+// cy.get('#ext-container-13', { timeout: 10000 })
+//   .should('not.have.class', 'x-hidden-display'); // ou .should('be.visible')
 
   })
 
